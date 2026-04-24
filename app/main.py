ba83e8ee7794e5895c721dff232e9b6fcdbb5f03
cyrus <cyruskrispin@gmail.com>
2026-04-24 22:15:11 +0800

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -7,9 +7,11 @@ def main():
         user_input = input("$ ").split()
         if user_input[0] == "exit":
             sys.exit()
-        if user_input[0] == "echo":
-            if user_input[1:]:
-                sys.stdout.write(" ".join(user_input[1:]) + "\n")
+        elif user_input[0] == "echo":
+            sys.stdout.write(" ".join(user_input[1:]) + "\n")
+        else:
+            sys.stdout.write(" ".join(user_input) + ": command not found" + "\n")
+        
         
 if __name__ == "__main__":
     main()
