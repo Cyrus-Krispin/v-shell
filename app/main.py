8c16a861591bc9f5606ae82737325f1aa9fc2b1d
cyrus <cyruskrispin@gmail.com>
2026-04-24 22:33:44 +0800

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -3,14 +3,26 @@ import sys
 
 def main():
 
+    builtin = {"type", "echo", "exit"}
+
     while True:
         user_input = input("$ ").split()
-        if user_input[0] == "exit":
-            sys.exit()
-        elif user_input[0] == "echo":
-            sys.stdout.write(" ".join(user_input[1:]) + "\n")
-        else:
-            sys.stdout.write(" ".join(user_input) + ": command not found" + "\n")
+        command = user_input[0]
+        match command:
+            case "exit":
+                sys.exit()
+            case "echo":
+                sys.stdout.write(" ".join(user_input[1:]) + "\n")
+            case "type":
+                for val in user_input[1:]: 
+                    if val in builtin:
+                        sys.stdout.write(val + " is a shell builtin" + "\n") 
+                    else:
+                        sys.stdout.write(val + ": not found" + "\n")
+
+ 
+            case _:
+                sys.stdout.write(command + ": command not found" + "\n")
         
         
 if __name__ == "__main__":
