b6478d5cc24dac90378e116a56352d66b9409596
Cyrus <cyruskrispin@gmail.com>
2026-04-26 13:13:23 +0800

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -52,7 +52,9 @@ def main():
  
             case "cd":
                 if user_input[1]:
-                    if os.path.isdir(user_input[1]):
+                    if user_input[1] == "~":
+                        os.chdir(os.getenv('HOME'))
+                    elif os.path.isdir(user_input[1]):
                         os.chdir(user_input[1])
                     else:
                         sys.stdout.write("cd: " + user_input[1] + ": No such file or directory" + "\n") 
